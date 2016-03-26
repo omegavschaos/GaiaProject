1e5abb6fbcbe55622c6f393283e5fa0d211233d4
JulienGuenard <julienguenard01@gmail.com>
2016-03-26 17:28:04 +0100

    Menu Principal:
    • Bouton Jouer fonctionnel
    • Bouton A propos fonctionnel
    • Bouton Quitter fonctionnel

---
+++ GaiaProject/Assets/Scripts/UI/Boutons_Interactions.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boutons_Interactions : MonoBehaviour {
+
+	void Jouer () {
+		Debug.Log ("aaa");
+		Application.LoadLevel("Ecran_Jeu");
+	}
+
+	void A_propos () {
+		Debug.Log ("bbb");
+		Application.LoadLevel("Ecran_Propos");
+	}
+
+	void Quitter () {
+		Debug.Log ("ccc");
+		Application.Quit();
+	}
+}
